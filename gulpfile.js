5123aefb58d366f13bc9c4c8c2b603f288d8fa5f
Nishi Chauhan <nishichauhan1811@gmail.com>
2018-06-06 11:29:31 +0530

    Correct Path of partials Scss

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var reload = browserSync.reload();
 
 gulp.task('sass', function () {
   gulp.src('./src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({includePaths: ['./src/css/']}).on('error', sass.logError))
     .pipe(gulp.dest('./src/css/'));
 });
 
